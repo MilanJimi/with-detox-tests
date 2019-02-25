5c8eba62591ebbe063f6b41d2409d4d5409aa17c
Milan Smolík <smolik.mil@gmail.com>
2019-02-25 11:36:39 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends Component {
   state = {
@@ -17,39 +17,11 @@ export default class App extends Component {
           alignItems: 'center',
         }}>
         <Text style={{ fontSize: 25, marginBottom: 30 }} testID="welcome">Welcome</Text>
-        <TouchableOpacity
-          testID="hello_button"
-          onPress={this.onButtonPress.bind(this, 'Hello')}>
-          <Text style={{ color: 'blue', marginBottom: 20 }}>Say Hello</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          testID="world_button"
-          onPress={this.onButtonPress.bind(this, 'World')}>
-          <Text style={{ color: 'blue', marginBottom: 20 }}>Say World</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-  renderAfterButton() {
-    return (
-      <View
-        style={{
-          flex: 1,
-          paddingTop: 20,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Text style={{ fontSize: 25 }}>{this.state.greeting}!!!</Text>
+        
       </View>
     );
   }
 
-  onButtonPress(greeting) {
-    this.setState({
-      greeting: greeting,
-    });
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
